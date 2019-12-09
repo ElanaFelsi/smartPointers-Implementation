611dd0579deb3a2d6c594c0abf487bfb563844ad
elana <elanaehrman@gmail.com>
2019-12-09 23:56:54 +0200

    dtor dsnt work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "sharedPointer.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SharedPtr<int> ptr1(new int);
+    SharedPtr<int> ptr2(new int);
+    ptr1 = ptr2;
     return 0;
 }
--- sharedPointer.h
+++ sharedPointer.h
@@ -10,15 +10,22 @@ class SharedPtr
 {
 public:
     explicit SharedPtr(T* ptr = NULL);
-    SharedPtr(SharedPtr<T> &sp);
     ~SharedPtr();
 
-    T &operator*() const;
-    T &operator->() const;
-
-    SharedPtr<T> &operator=(const T *ptr);
+    SharedPtr(const SharedPtr<T> &other);
+    SharedPtr<T> &operator=(const SharedPtr<T> &other);
 
 
+    T &operator*();
+    T &operator->();
+
+
+/*    SharedPtr<T> &operator=(const SharedPtr<T> &sp);
+    template <typename U>
+    SharedPtr<T> &operator=(const SharedPtr<U> &sp); /* for polymorphism Base=Derived */
+
+    /*template <typename U>
+    friend class SharedPtr;*/
 
 private:
     T* m_ptr;
@@ -31,6 +38,60 @@ SharedPtr<T>::SharedPtr(T *ptr): m_ptr(ptr), m_ReferenceCount(new ReferenceCount
     if(ptr)
         m_ReferenceCount->Increment();
     std::cout << "Created smart_ptr! Ref count is " << m_ReferenceCount->GetCount() << std::endl;
+}
+
+template<typename T>
+inline SharedPtr<T>::~SharedPtr()
+{
+    if(m_ReferenceCount)
+    {
+        int decCount = m_ReferenceCount->Decrement();
+        std::cout << "Destroyed! ref count is " << decCount << std::endl;
+    }
+    else
+    {
+        delete m_ReferenceCount;
+        delete m_ptr;
+
+        m_ReferenceCount = NULL;
+        m_ptr = NULL;
+    }
+}
+
+template<typename T>
+inline SharedPtr<T>::SharedPtr(const SharedPtr<T> &other): m_ptr(other.m_ptr), m_ReferenceCount(other.m_ReferenceCount)
+{
+    m_ReferenceCount ->Increment();
+    std::cout << "Copied! ref count is " << m_ReferenceCount->GetCount() << std::endl;
+}
+
+template<typename T>
+inline SharedPtr<T> &SharedPtr<T>::operator=(const SharedPtr<T> &other)
+{
+    if(this != &other)
+    {
+        if(m_ReferenceCount && m_ReferenceCount->Decrement() == 0)
+        {
+            delete m_ReferenceCount;
+            delete m_ptr;
+        }
+
+        m_ptr = other.m_ptr;
+        m_ReferenceCount = other.m_ReferenceCount;
+        m_ReferenceCount->Increment();
+    }
+    std::cout << "assigning =  ref count is " << m_ReferenceCount->GetCount() << std::endl;
+    return *this;
+}
+
+template<typename T>
+inline T &SharedPtr<T>::operator*() {
+    return *m_ptr;
+}
+
+template<typename T>
+inline T &SharedPtr<T>::operator->() {
+    return m_ptr;
 }
 
 #endif //MEMORY_SMART_POINTERS_ELANAFELSI_SHAREDPOINTER_H
